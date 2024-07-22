87f91ec2f920bc60997d286e68a4523b7da6cecf
andxbes <andxbes@gmail.com>
2024-07-22 20:51:29 +0300

    256

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -5,6 +5,10 @@ export default function Login() {
     email:'',
     password: ''
   });
+
+
+  const emailIsInvalid = enteredValues.email !== '' && !enteredValues.email.includes('@') ;
+
   function handleSubmit(e){
     e.preventDefault();
     console.info('submitted', enteredValues?.email, enteredValues?.password);
@@ -30,6 +34,9 @@ export default function Login() {
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" value={enteredValues?.email??''}  onChange={(event)=>handleInputChange('email', event.target.value)} />
+          <div className="control-error" >
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
